7bb9486c4a6651895643d6c8206d5a100a99c8c4
Luan Severo <luansevero@icloud.com>
2022-07-07 16:46:54 -0300

    feat: Adding link to the footer(need to improve that)

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,24 +1,32 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export default function Footer() {
+export default function Footer({page}) {
     return (
         <Container>
             <LinksSections>
+                <Link to="/">
                     <ion-icon name="storefront-outline"></ion-icon>
                     <h2>Loja</h2>
+                </Link>
             </LinksSections>
             <LinksSections>
+                <Link to="/carrinho">
                     <ion-icon name="cart-outline"></ion-icon>
                     <h2>Carrinho</h2>
+                </Link>
             </LinksSections>
             <LinksSections>
+                <Link to="/pedidos">
                     <ion-icon name="receipt-outline"></ion-icon>
                     <h2>Pedidos</h2>
+                </Link>
             </LinksSections>
             <LinksSections>
+                <Link to="/menu">
                     <ion-icon name="person-outline"></ion-icon>
                     <h2>Menu</h2>
+                </Link>
             </LinksSections>
         </Container>
     )
@@ -38,6 +46,7 @@ const Container = styled.footer`
     padding: 15px;
 `
 const LinksSections = styled.section`
+    a{
         color: inherit;
         text-decoration: inherit;
         height:100%;
@@ -46,7 +55,7 @@ const LinksSections = styled.section`
         align-items: center;
         justify-content: space-between;
         ion-icon{
-            margin-bottom: 10px;
+            margin-bottom: 5px;
             width:30px;
             height:30px;
         }
@@ -56,6 +65,7 @@ const LinksSections = styled.section`
             line-height: 17px;
             color: #000000;
         }
+    }
 `
 
 //Depois mover reset do button para global style;
