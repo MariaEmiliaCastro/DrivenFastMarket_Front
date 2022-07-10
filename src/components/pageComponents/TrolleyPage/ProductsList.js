404079f7540405f62dbd08e4f47eb7afe825307d
Luan Severo <luansevero@icloud.com>
2022-07-10 19:17:06 -0300

    feat: Product map, with buttons

--- src/components/pageComponents/TrolleyPage/ProductsList.js
+++ src/components/pageComponents/TrolleyPage/ProductsList.js
@@ -1,10 +1,54 @@
 import { useState } from "react";
+import { useContext } from "react";
+
+import TokenContext from "../../../contexts/TokenContext";
+
 import { ItensList } from "./TrolleyStyle";
 
 export default function ProductsList({ product, handleCostumerCart }) {
-    const [product, setProduct] = useState(product);
+    const [item, setItem] = useState(product);
+    const [waitClick, setWaitClick] = useState(false);
     
-    function handleProductChange(){
+    function handleClick(type){
+        const { token } = useContext(TokenContext);
+        setWaitClick(true);
+        
+        const clickType = {
+            adding: async () => {
+                try{
+                    const promisse = axios.put(`${url}carrinho`, item, token);
+                    promisse.then(() => {
+                        handleCostumerCart()
+                    })
+                }catch(error){
+                    console.log(error)
+                }
+            },
+            reduce: async () => {
+                try{
+                    const promisse = axios.put(`${url}carrinho`, item, token);
+                    promisse.then(() => {
+                        handleCostumerCart()
+                    })
+                }catch(error){
+                    console.log(error)
+                }
+            },
+            delete: async () => {
+                try{
+                    const promisse = axios.delete(`${url}carrinho`, item, token);
+                    promisse.then(() => {
+                        handleCostumerCart()
+                    })
+                }catch(error){
+                    console.log(error)
+                }
+            }
+        }
+        
+        const buttonAction = clickType[type];
+        buttonAction();
+
     }
     
 
@@ -20,11 +64,11 @@ export default function ProductsList({ product, handleCostumerCart }) {
             <div className="ProductHandle">
                 {
                     product.amount === 1
-                    ?   <ion-icon name="remove-outline"></ion-icon>
-                    :   <ion-icon name="remove-outline"></ion-icon>
+                    ?   <ion-icon disabled={waitClick} name="trash-outline" onClick={() => handleClick("delete")}></ion-icon>
+                    :   <ion-icon disabled={waitClick} name="remove-outline" onClick={() => handleClick("reduce")}></ion-icon>
                 }
                 <span>{amount}</span>
-                <ion-icon name="add-outline"></ion-icon>
+                <ion-icon disabled={waitClick} name="add-outline" onClick={() => handleClick("adding")}></ion-icon>
             </div>
         </ItensList>
     )
